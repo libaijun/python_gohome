a9db4bdd0e4ef2f81aff6a1d7b88cbaae8adc34d
libojun <libj@igoldenbeta.com>
2019-08-08 10:57:30 +0800

    增加了日志

--- GoHome.py
+++ GoHome.py
@@ -3,9 +3,14 @@ import os
 from apscheduler.schedulers.blocking import BlockingScheduler
 from datetime import datetime
 
+import MyLogging
+
 
 # r'' 防止路径带空格
 config = {'process_name': 'TeamViewer.exe', 'process_path': r'"C:/Program Files (x86)/TeamViewer/TeamViewer.exe"'}
+
+# 创建logger
+logger = MyLogging.my_logger
 
 
 # 查询指定的程序的pid
@@ -16,7 +21,7 @@ def get_process_pid(process_name):
             if psutil.Process(pid).name() == process_name:
                 return pid
         except psutil.NoSuchProcess:
-            print("%s 没有该进程: %s" % (detail_now(), pid))
+            logger.error("%s 没有该进程: %s" % (detail_now(), pid))
 
     else:
         return None
@@ -25,9 +30,9 @@ def get_process_pid(process_name):
 def start_process():
     flag = get_process_pid(config['process_name'])
     if flag is not None:
-        print("%s 程序【%s】已启动, pid = %d " % (detail_now(), config['process_name'], flag))
+        logger.info("%s 程序【%s】已启动, pid = %d " % (detail_now(), config['process_name'], flag))
     else:
-        print("%s 程序【%s】未启动, 启动之" % (detail_now(), config['process_name']))
+        logger.info("%s 程序【%s】未启动, 启动之" % (detail_now(), config['process_name']))
         os.startfile(config['process_path'])
 
 
@@ -36,7 +41,7 @@ def detail_now():
 
 
 if __name__ == '__main__':
-    print('启动python脚本......')
+    logger.info('启动python脚本......')
     scheduler = BlockingScheduler()
     # 工作日
     scheduler.add_job(start_process, 'cron', day_of_week='0-4', hour='16-23,0-9', minute='*/1', second='0')
--- MyLogging.py
+++ MyLogging.py
@@ -2,4 +2,51 @@
     日志配置
 """
 import logging
+from datetime import datetime
+import os
 
+
+class MyLogging:
+
+    def __init__(self):
+        time_str = datetime.now().strftime('%Y-%m-%d')
+
+        lib_path = os.path.abspath(os.path.join(os.path.dirname(__file__), './logs'))
+
+        exists = os.path.exists(lib_path)
+        if not exists:
+            print('不存在，创建目录 %s ' % lib_path)
+            os.mkdir(lib_path)
+
+        # 日志格式  os.sep: 路径分隔符 \ /
+        filename = lib_path + os.sep + time_str + ".log"
+
+        # 默认root
+        self.logger = logging.getLogger('lbj')
+
+        # 等级
+        self.logger.setLevel(logging.INFO)
+
+        # 输出取控制台
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
+
+        # 打印日志到文件
+        file_handler = logging.FileHandler(filename=filename)
+        file_handler.setLevel(logging.INFO)
+
+        # 设置格式对象
+        formatter = logging.Formatter(
+            "%(asctime)s %(filename)s[line:%(lineno)d]%(levelname)s - %(message)s")
+
+        # 设置handler的格式对象
+        console_handler.setFormatter(formatter)
+        file_handler.setFormatter(formatter)
+
+        # 加到logger中
+        self.logger.addHandler(console_handler)
+        self.logger.addHandler(file_handler)
+
+
+# 暴露一个日志对象
+my_logger = MyLogging().logger
